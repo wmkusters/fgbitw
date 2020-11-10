00dc15ef28600fa3a37cd34c6a5294a08f37da94
Bill Kusters <wmkusters@me.com>
2020-11-10 00:19:17 -0500

    not working yet

--- sources_1/new/game_fsm.sv
+++ sources_1/new/game_fsm.sv
@@ -25,39 +25,59 @@ module game_fsm(
                 input logic [7:0] move,
                 output logic [1:0] board [8:0][8:0]
     );
-    parameter RESET_IN = 8'b0;
+    parameter RESET_IN = 8'b0000_0000;
     parameter MOVE_READY = 8'b0000_0001;
+    parameter CYC_BOARDS = 8'b0000_0010;
     
+    parameter BRD_CNT = 5;
     parameter [1:0] b = 2'b01;
     parameter [1:0] w = 2'b10;
     parameter [1:0] e = 2'b00;
-    parameter [1:0] EMPTY_BOARD [8:0][8:0] ='{'{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e},
-                                              '{e, e, e, e, e, e, e, e, e}};
-                                              
-//    parameter BOARD1 = {9{3'b0}, 9{3'b0}, };
+    logic [1:0] EMPTY_BOARD [8:0][8:0] =  '{'{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e}};
+    logic [1:0] CORNER_BOARD [8:0][8:0] = '{'{e, b, e, e, e, e, e, e, e},
+                                            '{b, b, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, e, e},
+                                            '{e, e, e, e, e, e, e, w, w},
+                                            '{e, e, e, e, e, e, e, w, e}};
     
+                                              
     logic [1:0] next_board [8:0][8:0];
     logic [7:0] input_state;
+    logic [3:0] count;
+    
+    assign next_board = EMPTY_BOARD;
     
     always_comb begin
         if (reset) input_state = RESET_IN;
         else if (move_avail) input_state = MOVE_READY;
+        else input_state = CYC_BOARDS;
     end
     
     always_ff @(posedge clk_in) begin
         case (input_state)
             RESET_IN  : begin
                             board <= EMPTY_BOARD;
+                            count <= BRD_CNT;
                         end
             MOVE_READY: begin
                             board <= next_board;
+                        end
+            CYC_BOARDS: begin
+                            if (count > 0) begin
+                                
+                            end
                         end
         endcase
     end
