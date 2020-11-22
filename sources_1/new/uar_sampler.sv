c4f936457055729290ed9e0ccacbf4e8e1fbff4f
jrtalbot <joshuartalbot@comcast.net>
2020-11-22 13:29:39 -0500

    rx clk changed to 65MHz

--- sources_1/new/uar_sampler.sv
+++ sources_1/new/uar_sampler.sv
@@ -27,7 +27,7 @@ module uar_sampler(
     output logic sig_out
     );
     
-    parameter CLK_HZ = 100_000_000;
+    parameter CLK_HZ = 65_000_000;
     parameter SAMP_PER_BIT = 16;
     parameter BAUD_RATE = 9600;
     
