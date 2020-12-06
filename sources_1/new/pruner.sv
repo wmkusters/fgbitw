a359f76eb107d9b3528d9f56218620f57c30a229
Bill Kusters <wmkusters@me.com>
2020-12-05 23:39:09 -0500

    added blank pruner

---
+++ sources_1/new/pruner.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/05/2020 11:01:40 PM
+// Design Name: 
+// Module Name: pruner
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pruner(
+
+    );
+endmodule
