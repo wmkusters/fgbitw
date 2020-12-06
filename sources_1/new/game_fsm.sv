cf2074faa1d809777a4d3290359dbf61d7bc1e25
Bill Kusters <wmkusters@me.com>
2020-12-05 22:56:49 -0500

    py strings, midchange fsm

--- sources_1/new/game_fsm.sv
+++ sources_1/new/game_fsm.sv
@@ -55,7 +55,18 @@ module game_fsm(
                                               
     logic [1:0] own_board [8:0][8:0];
     logic [7:0] input_state;
-    logic prune55, prune54, prune56, prune65, prune45;
+    logic wire_44_45, wire_44_54, wire_45_55, wire_34_44, wire_35_45, wire_43_44, wire_45_46;  
+    logic hot;
+    assign hot = 1;
+    wire_calc calc_44_45(.tile1(own_board[4][4]),
+                         .tile2(own_board[4][5]),
+                         .neighbor1(wire_34_44),
+                         .neighbor2(wire_35_45),
+                         .neighbor3(wire_43_44),
+                         .neighbor4(wire_45_46),
+                         .neighbor5(wire_44_54),
+                         .neighbor6(wire_45_55)
+    );                     
     prune_tile tile55(.self(own_board[5][5]),
                       .up(own_board[4][5]),
                       .up_prune(prune45),
@@ -64,7 +75,8 @@ module game_fsm(
                       .left(own_board[5][4]),
                       .left_prune(prune54),
                       .right(own_board[5][6]),
-                      .right_prune(prune56))
+                      .right_prune(prune56)
+                      .prune(prune55));
 
     always_comb begin
 //        if (reset) input_state = RESET_IN;
@@ -91,29 +103,49 @@ module game_fsm(
                           end
             default: board <= board;
         endcase
+        if (pulsed) begin
+          prune55_reg <= 1;
+          prune54_reg <= 1;
+        end else begin
+          prune55_reg <= prune55;
+        end 
     end
 endmodule
 
-module prune_tile(
-                  input [1:0] self, up, down, left, right,
-                  input up_prune, left_prune, right_prune, down_prune,
-                  output prune
-       );
-  always_comb begin 
-    if ((self == e) ||
-        (up   == e) ||
-        (down == e) ||
-        (left == e) ||
-        (right == e)) begin
-      prune = 0;
-    end else if (((up == self) & (up_prune)) ||
-                 ((down == self) & (down_prune)) ||
-                 ((left == self) & (left_prune)) ||
-                 ((right == self) & (right_prune))) begin
-      prune = (up_prune || down_prune || left_prune || right_prune); 
+module wire_calc(input tile1, tile2,
+                 input neighbor1, neighbor2, neighbor3, neighbor4, neighbor5, neighbor6,
+                 output wire_out
+  );
+  always_comb begin
+    if ((tile1 ^ tile2) == 2'b11) begin
+      wire_out = 0;
+    end else if ((tile1 == e) | (tile2 == e)) begin
+      wire_out = 1;
     end else begin
-      prune = 0;
-    end 
-  end
+      wire_out = (neighbor1 | neighbor2 | neighbor3 | neighbor4 | neighbor5 | neighbor6);
+    end
 endmodule
+
+// module prune_tile(
+//                   input [1:0] self, up, down, left, right,
+//                   input up_prune, left_prune, right_prune, down_prune,
+//                   output prune
+//        );
+//   always_comb begin 
+//     if ((self == e) ||
+//         (up   == e) ||
+//         (down == e) ||
+//         (left == e) ||
+//         (right == e)) begin
+//       prune = 0;
+//     end else if (((up == self) & (~up_prune)) ||
+//                  ((down == self) & (~down_prune)) ||
+//                  ((left == self) & (~left_prune)) ||
+//                  ((right == self) & (~right_prune))) begin
+//       prune = ~(up_prune || down_prune || left_prune || right_prune); 
+//     end else begin
+//       prune = 1;
+//     end 
+//   end
+// endmodule
   
